595eb13f04cfcaa8cdffc6bf730572c40d600fdd
Bintang fauzan <76691158+BintangFauzan@users.noreply.github.com>
2022-06-26 22:26:23 +0700

    Update all.php

--- riwayat/all.php
+++ riwayat/all.php
@@ -1,11 +1,14 @@
 <?php 
 include '../koneksi.php';
 
+header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Headers:*');
+
 $query = "select * from riwayat";
 
 $statement = $dbConn -> query($query);
 $statement->setfetchMode(PDO::FETCH_OBJ);
-$result = $statement->fetchALL();
+$result["riwayat"] = $statement->fetchALL();
 header("Content-Type: application/json; charset=UTF-8");
 echo json_encode($result);
 
