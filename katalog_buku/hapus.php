ca6ecfb5ef3aaeb940e736942b638104061500d7
Bintang fauzan <76691158+BintangFauzan@users.noreply.github.com>
2022-06-26 22:24:12 +0700

    Update hapus.php

--- katalog_buku/hapus.php
+++ katalog_buku/hapus.php
@@ -1,83 +1,20 @@
-<?php 
+<?php
 include '../koneksi.php';
-$response = [
-    'status' => false,
-    'error' => '',
-    'data' => []
-];
 
-if($_SERVER['REQUEST_METHOD'] !== 'DELETE'){
-    header('Content-Type: application/json');
-    http_response_code(400);
-    $response['error'] = 'DELETE method required';
-    echo json_encode($response);
-    exit();
+header("Content-Type: application/json; charset=UTF-8");
+
+$username = $_POST['username'];
+
+$query =  "DELETE FROM `user` WHERE username = $username";
+$execute = $dbConn->query($query);
+$response = [];
+
+if ($execute) {
+    $response['status'] = 'succcess';
+    $response['message'] = 'data berhasil dihapus';
+} else {
+    $response['status'] = 'failed';
+    $response['message'] = 'data gagal dihapus';
 }
-
-/**
- * Get input data from RAW data
- */
-$data = file_get_contents('php://input');
-if($data === false || empty($data)){
-    header('Content-Type: application/json');
-    http_response_code(400);
-    $response['error'] = 'Form data tidak tersedia';
-    echo json_encode($response);
-    exit();
-}
-/*
- * Parse data form ke dalam array
- */
-parse_str($data, $res);
-$isbn = $res['isbn'] ?? '';
-
-/**
- *
- * Cek apakah ISBN tersedia
- */
-try{
-    $queryCheck = "SELECT * FROM katalog_buku where isbn = :isbn";
-    $statement = $dbConn->prepare($queryCheck);
-    $statement->bindValue(':isbn', $isbn);
-    $statement->execute();
-    $row = $statement->rowCount();
-    /**
-     * Jika data tidak ditemukan
-     * rowcount == 0
-     */
-    if($row === 0){
-        header('Content-Type: application/json');
-        $response['error'] = 'Data tidak ditemukan ISBN '.$isbn;
-        echo json_encode($response);
-        http_response_code(400);
-        exit(0);
-    }
-}catch (Exception $exception){
-    header('Content-Type: application/json');
-    $response['error'] = $exception->getMessage();
-    echo json_encode($response);
-    http_response_code(400);
-    exit(0);
-}
-
-/**
- * Hapus data
- */
-try{
-    $queryCheck = "DELETE FROM katalog_buku where isbn = :isbn";
-    $statement = $dbConn->prepare($queryCheck);
-    $statement->bindValue(':isbn', $isbn);
-    $statement->execute();
-    $response['status'] = true;
-}catch (Exception $exception){
-    header('Content-Type: application/json');
-    $response['error'] = $exception->getMessage();
-    echo json_encode($response);
-    http_response_code(400);
-    exit(0);
-}
-
-header('Content-Type: application/json');
-echo json_encode($response);
-
-?>
+$json = json_encode($response, JSON_PRETTY_PRINT);
+echo $json;
